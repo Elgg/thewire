b4e7869e569dd1f33dfa9e4995f567c442edadcd
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-03 17:53:05 +0000

    Pulled in the interface changes.
    git-svn-id: http://code.elgg.org/elgg/trunk@5257 36083f99-b078-4883-b0ff-0f9b5a30f544

--- index.php
+++ index.php
@@ -1,36 +1,38 @@
 <?php
-/**
- * Elgg index page for web-based applications
- *
- * @package Elgg
- * @subpackage Core
- * @author Curverider Ltd
- * @link http://elgg.org/
- */
 
-/**
- * Start the Elgg engine
- */
-define('externalpage',true);
-require_once(dirname(__FILE__) . "/engine/start.php");
-
-if (!trigger_plugin_hook('index', 'system', null, FALSE)) {
 	/**
-	 * Check to see if user is logged in, if not display login form
-	 **/
+	 * Elgg thewire index page
+	 * 
+	 * @package Elggthewire
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider <info@elgg.com>
+	 * @copyright Curverider Ltd 2008-2010
+	 * @link http://elgg.com/
+	 */
 
-	if (isloggedin()) {
-		forward('pg/dashboard/');
-	}
-
-	//Load the front page
-	global $CONFIG;
-	$title = elgg_view_title(elgg_echo('content:latest'));
-	set_context('search');
-	$content = elgg_list_registered_entities(array('limit' => 10, 'full_view' => FALSE, 'allowed_types' => array('object','group')));
-	set_context('main');
-	global $autofeed;
-	$autofeed = FALSE;
-	$content = elgg_view_layout('two_column_left_sidebar', '', $title . $content, elgg_view("account/forms/login"));
-	page_draw(null, $content);
-}
+	// Load Elgg engine
+		require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+		
+	// Get the current page's owner
+		$page_owner = page_owner_entity();
+		if ($page_owner === false || is_null($page_owner)) {
+			$page_owner = $_SESSION['user'];
+			set_page_owner($page_owner->getGUID());
+		}
+		
+	// title
+	    $area2 = elgg_view_title(elgg_echo("thewire:read"));
+	    
+	//add form
+		$area2 .= elgg_view("thewire/forms/add");
+	    
+	// Display the user's wire
+		$area2 .= list_user_objects($page_owner->getGUID(),'thewire'); // elgg_view("thewire/view",array('entity' => $thewire));
+    
+    //select the correct canvas area
+	    $body = elgg_view_layout("two_column_left_sidebar", '', $area2);
+		
+	// Display page
+		page_draw(sprintf(elgg_echo('thewire:user'),$page_owner->name),$body);
+		
+?>
